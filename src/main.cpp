79a67d90fe34788b24871eb25f791edacf475f66
Accacio <raccacio@poli.ufrj.br>
2020-08-18 22:20:44 +0200

    add clear color

--- src/main.cpp
+++ src/main.cpp
@@ -285,8 +285,8 @@ int main(int argc, char *argv[]) {
         ImGui::Render();
         glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
 
-        // ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-        // glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
         if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
         {
